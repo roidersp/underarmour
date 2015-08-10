d2686337bf4c6b8977301d440965f8de4c448873
Rodrigo Luis Santander <roidersp@gmail.com>
2015-08-10 11:42:47 -0500

    gyu

---
+++ js/indepth.js
@@ -0,0 +1,157 @@
+var disqus_shortname = 'juanfutbol';
+var disqus_identifier;
+var disqus_url="test";
+var disqus_number_c=2;
+var disqus_per_page=3;
+var tamaño_total=1920;
+
+$(document).on("click", "#indepth_cover" ,function(){
+		var position = $(".indepth_content_top").position();
+		$('html, body').animate({
+			scrollTop: position.top
+		}, 2000);
+	});
+
+var indepth_sizeAdjust = function(firstTime){
+	$(".indepth_page").each(function(){
+		if($(this).attr("resize") == "true"){
+			var h = parseInt($(this).width(),10) / $(this).attr("width") * $(this).attr("height");
+			$(this).css("height", h + "px");
+		}else if(firstTime && $(this).attr("resize") == "false"){
+			$(".indepth_background", $(this)).css("min-width", $(this).attr("width") + "px");
+			$(this).css("height", $(this).attr("height") + "px");
+		}
+	})
+}
+
+var indepth_preloadImgs = function(){
+	$("img[over]").each(function(){
+		$(this).attr("out", $(this).attr("src"));
+		$(this).on("mouseenter", function(){
+			$(this).attr("src", $(this).attr("over"));
+		}).on("mouseleave", function(){
+			$(this).attr("src", $(this).attr("out"));
+		}).css("cursor", "pointer");
+
+		var tmp = $("<img/>");
+		tmp.attr("src", $(this).attr("over"));
+		tmp.css({"position":"absolute", "top":"-9999px", "left":"-9999px"})
+		tmp.appendTo("body");
+	});
+}
+
+  var detect_mobile=function(){
+	 var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+ };
+ 
+	var mobile=false;
+	
+	 if (isMobile.Android())
+	 {
+	 mobile=true;
+	 }
+	 else if (isMobile.BlackBerry())
+	 {
+	 mobile=true;
+	 }
+	 else if (isMobile.iOS())
+	 {
+	 mobile=true;
+	 }
+	 else if (isMobile.Opera())
+	 {
+	 mobile=true;
+	 }
+	 else if (isMobile.Windows())
+	 {
+	 mobile=true;
+	 }
+	 else
+	 {
+	 mobile=false;
+ }
+	 return mobile;
+ }
+ 
+function loadDisqus(source, identifier, url) {
+if (window.DISQUS) {
+   jQuery('#disqus_thread').insertAfter(source);
+   /** if Disqus exists, call it's reset method with new parameters **/
+
+    DISQUS.reset({
+  reload: true,
+  config: function () { 
+   this.page.identifier = identifier.toString();    //important to convert it to string
+   this.page.url = url;
+  }
+ });
+} else {
+//insert a wrapper in HTML after the relevant "show comments" link
+	source.append('<div id="disqus_thread"></div>');
+   //jQuery('<div id="disqus_thread"></div>').insertAfter(source);
+   disqus_identifier = identifier; //set the identifier argument
+   disqus_url = url; //set the permalink argument
+   disqus_per_page=3;
+   //append the Disqus embed script to HTML
+   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+   dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+   jQuery('head').append(dsq);
+}
+};
+
+loadDisqus($("#indepth_coments"),disqus_url, "http://juanfutbol.com/indepth/"+disqus_url);
+
+
+
+
+
+$(document).ready(function(){
+	indepth_sizeAdjust(true);
+	indepth_preloadImgs();
+	var ventana_alto = $(window).height();
+
+    	$('#indepth_cover').css("height",(ventana_alto)+"px");
+   
+
+
+
+    
+    if(navigator.platform == 'iPad'){
+	    //$("#indepth_parallax_back").css("background-size", "100%");
+	    $("#indepth_parallax_back").css("background-attachment", "initial");
+	    console.log("ipad")
+    }
+});
+
+$(window).on("resize", function(){
+	indepth_sizeAdjust(false);
+	var ventana_alto = $(window).height();
+    	$('#indepth_cover').css("height",(ventana_alto)+"px");
+    
+	 if(navigator.platform == 'iPad'){
+	    //$("#indepth_parallax_back").css("background-size", "100%");
+	    $("#indepth_parallax_back").css("background-attachment", "initial");
+    }
+	 if(navigator.platform == 'iPad' || navigator.platform == 'iPhone' || navigator.platform == 'iPod' || navigator.platform == 'Android')
+    { 
+    }else{
+    	var ventana_alto = $(window).height();
+	 //ventana_alto=ventana_alto-(ventana_alto*.15)
+	 	//$('.indepth_anuncio_section').css("height",ventana_alto-(ventana_alto*.10)+"px");
+    }
+})
